ab18e11b49cebae073d86d64bfc6051cf563cec7
stephenboles <stephenboles@gmail.com>
2017-08-26 07:06:42 -0700

    Add existing file

---
+++ variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+double temp=75, weight=216, age=33;
+ 
+
+int main()
+{
+
+    cout << "Your temp is "  << temp <<endl;
+    cout << "Your weight is " << weight <<endl;
+    cout << "Your temp is " << age << endl;
+    return 0;
+}
