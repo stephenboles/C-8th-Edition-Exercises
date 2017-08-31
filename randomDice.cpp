0e36a25d3bf2c773e58c90a5648e237f413889ad
stephenboles <stephenboles@gmail.com>
2017-08-30 20:34:48 -0700

    Random Dice Thrower

--- randomDice.cpp
+++ randomDice.cpp
@@ -11,9 +11,10 @@ int main()
 
     const int MIN_VALUE = 1;
     const int MAX_VALUE = 6;
-    int y = (rand() % (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
     int x = (rand() % (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
+    int y = (rand() % (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
     
+    cout << rand() << endl;
     cout << rand() << endl;
     cout << "Your two random numbers are " << x << " and " << y << ".";
 
